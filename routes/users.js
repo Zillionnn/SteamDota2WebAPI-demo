a89912d0ebc33a4b20af6d186eebe1a5f19eb5c2
Zillion <skyestzhang@163.com>
2018-03-08 00:09:00 +0800

    比赛详细

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var request=require('request');
+let logger=require('logger').createLogger('user.log');
+
+
 let CONSTANTS=require('../Constants/constant');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,6 +13,8 @@ router.get('/', function(req, res, next) {
 var  userSummeries='http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='+CONSTANTS.key+'&steamids=76561198081585830';
 var RecentlyPlayedGames='http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1?key='+CONSTANTS.key+'&steamid=76561198081585830';
 let getMatchHistory='http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1?key='+CONSTANTS.key+'&account_id=76561198081585830&hero_id=74';
+let getMatchDetail='http://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v1?key='+CONSTANTS.key+'&match_id=';
+
 
 router.get('/userinfo',function (req, res, next) {
   console.log("userinfo");
@@ -43,9 +48,28 @@ router.get('/getMatchHistoryByHero',function (req, res, next) {
         if(err){
             console.log(err);
         }else{
-            console.log(data.body);
+           // console.log(data.body);
             res.send(data.body);
         }
     })
+});
+
+
+
+router.post('/getMatchDetail',function (req, res, next) {
+    console.log("get match detail");
+    console.log(req.body);
+    let match_id=parseInt(req.body.match_id);
+    let url=getMatchDetail+match_id;
+    console.log(url);
+    request(url,function (err,data) {
+        if(err){
+            console.log(err);
+        }else{
+            console.log(data.body);
+            res.send(data.body);
+        }
+
+    });
 });
 module.exports = router;
